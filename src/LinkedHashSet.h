a2e67e505d7d021eaafc0c19b12b4cac8638ac77
Eric Shelbogashev <e.shelbogashev@gmail.com>
2022-11-04 21:19:52 +0700

    Optimized find, some fixes, added more comments

--- src/LinkedHashSet.h
+++ src/LinkedHashSet.h
@@ -8,8 +8,7 @@
 template<typename T, typename Hasher=std::hash<T>>
 class LinkedHashSet {
 public:
-    // CR: move comments here
-    explicit LinkedHashSet(size_t capacity = DEFAULT_CAPACITY_);
+    explicit LinkedHashSet(size_t capacity = DEFAULT_CAPACITY);
 
     ~LinkedHashSet();
 
@@ -27,16 +26,16 @@ public:
 
     bool empty() const;
 
-    bool contains(const T &e) const;
+    bool contains(const T &e);
 
-    /* Restore hashset to original form (including size and capacity. */
+    /* Restore hashset to original form (including size and capacity). */
     LinkedHashSet &clear();
 
     LinkedHashSet &operator=(LinkedHashSet other);
 
-    bool operator==(const LinkedHashSet &other);
+    bool operator==(LinkedHashSet &other);
 
-    bool operator!=(const LinkedHashSet &other);
+    bool operator!=(LinkedHashSet &other);
 
     typename std::list<T>::iterator find(const T &e);
 
@@ -57,34 +56,74 @@ private:
 
     private:
         friend LinkedHashSet;
-        typename std::list<T>::iterator iterator_;
-        T &value_;
+        typename std::list<T>::iterator _iterator;
+        T &_value;
     };
 
-    static const size_t DEFAULT_CAPACITY_ = 8;
-    constexpr static const double CAPACITY_COEFF_ = 0.85;
-    size_t arr_capacity_;
-    /* Container of buckets lists. */
-    std::list<Entry> **arr_;
-    /* history_ is a storage of elements;
-     * arr_ buckets contains an Entry with an iterator to the element and its pointer for O(1) fast access. */
-    std::list<T> history_;
-    Hasher hasher_;
-    /* Returns index of bucket list in arr_;
-     * Uses special Hasher class with () operator overridden to get hash and convert it to position in bucket array. */
-    inline size_t get_hash_pos_(const T &e, size_t capacity) const;
+    /* Default (size of _arr/number of buckets) on initialization. */
+    static const size_t DEFAULT_CAPACITY = 8;
 
-    inline void delete_arr_();
+    /* Coefficient which uses by public insert() and remove() for resizing _arr.
+     * Describes ratio of elements to _arrCapacity.
+     */
+    constexpr static const double CAPACITY_COEFF = 0.85;
 
-    /* Restore arr_ from history_ with new capacity;
-     * Overrides arr_ without delete[] for previous arr_; */
-    void resize_(size_t new_capacity);
+    /* Size of arr array, number of buckets. */
+    size_t _arrCapacity;
 
-    inline typename std::list<Entry>::iterator is_in_list_(std::list<Entry> *list, const T &e) const;
+    /* Container of buckets lists.
+     * _arr buckets contain an Entry with an iterator to the object and object's reference for O(1) fast access.
+     */
+    std::list<Entry> **_arr;
 
-    inline std::list<Entry> &get_list(std::list<Entry> **arr, size_t pos);
+    /* _history is a main storage of objects. */
+    std::list<T> _history;
 
-    void insert_(std::list<Entry> **arr, const T &e, typename std::list<T>::iterator &it, size_t capacity);
+    Hasher _hasher;
+
+    /* Returns an index of bucket in _arr, which contains Entry with e object.
+     *
+     * @param e         The object whose index is to be obtained. It will be hashed with std::hash().
+     * @param capacity  Hash division module.
+     */
+    inline size_t _getHashPos(const T &e, size_t capacity) const;
+
+    /* Deletes all buckets and _arr using delete and delete[] respectively.
+     * Doesn't change _arr to nullptr.
+     */
+    inline void _deleteArr();
+
+    /* Replaces _arr with a container of the same type with size newCapacity.
+     * Uses delete[] for previous _arr.
+     * Overwrites _arrCapacity with newCapacity.
+     *
+     * @param newCapacity   New _arrCapacity for _arr, which is needed after _resize().
+     */
+    void _resize(size_t newCapacity);
+
+    /* Returns an iterator of the list pointing to the Entry containing e.
+     *
+     * @param list  Entry list where will be searched Entry with e.
+     * @param e     Search object with type T.
+     */
+    inline typename std::list<Entry>::iterator _findInList(std::list<Entry> *list, const T &e) const;
+
+    /* Returns an Entry list from arr with position pos.
+     * If the list of Entry at index pos is nullptr, it will be initialized and placed in _arr at pos.
+     *
+     * @param arr   Bucket array. Must be initialized.
+     * @param pos   Bucket index. Must be in [0, _arrCapacity].
+     */
+    inline std::list<Entry> &_getList(std::list<Entry> **arr, size_t pos);
+
+    /* Puts T object from *it in the bucket from arr filling an Entry with a list<T> iterator it.
+     * Allocates a bucket in the arr if needed.
+     *
+     * @param arr       Bucket array. Must be initialized.
+     * @param it        An iterator pointing to the object being placed.
+     * @param capacity  arr size.
+     */
+    void _insert(std::list<Entry> **arr, typename std::list<T>::iterator &it, size_t capacity);
 };
 
 #include "LinkedHashSet.hpp"
--- src/LinkedHashSet.hpp
+++ src/LinkedHashSet.hpp
@@ -5,106 +5,105 @@
 #include <cassert>
 
 template<typename T, typename Hasher>
-LinkedHashSet<T, Hasher>::LinkedHashSet(size_t capacity) :  arr_capacity_(capacity),
-                                                            arr_(new std::list<Entry> *[arr_capacity_]()) {
+LinkedHashSet<T, Hasher>::LinkedHashSet(size_t capacity) :  _arrCapacity(capacity),
+                                                            _arr(new std::list<Entry> *[_arrCapacity]()) {
 }
 
 template<typename T, typename Hasher>
 LinkedHashSet<T, Hasher>::~LinkedHashSet() {
-    delete_arr_();
+    _deleteArr();
 }
 
 template<typename T, typename Hasher>
-LinkedHashSet<T, Hasher>::LinkedHashSet(const LinkedHashSet &other) : arr_capacity_(other.arr_capacity_),
-                                                                      history_(other.history_),
-                                                                      hasher_(other.hasher_),
-                                                                      arr_(nullptr) {
-    resize_(other.arr_capacity_);
+LinkedHashSet<T, Hasher>::LinkedHashSet(const LinkedHashSet &other) : _arrCapacity(other._arrCapacity),
+                                                                      _history(other._history),
+                                                                      _hasher(other._hasher),
+                                                                      _arr(nullptr) {
+    _resize(other._arrCapacity);
 }
 
 template<typename T, typename Hasher>
 LinkedHashSet<T, Hasher> &LinkedHashSet<T, Hasher>::operator=(LinkedHashSet other) {
-    if (*this == other)
+    if (*this == other) {
         return *this;
+    }
     swap(other);
     return *this;
 }
 
 template<typename T, typename Hasher>
 bool LinkedHashSet<T, Hasher>::insert(const T &e) {
-    if (this->size() >= arr_capacity_) {
-        resize_(arr_capacity_ * 2);
-    }
-
     if (contains(e)) {
         return false;
     }
-    auto it = history_.insert(history_.end(), e);
-    insert_(arr_, e, it, arr_capacity_);
+    if (this->size() >= _arrCapacity) {
+        _resize(_arrCapacity * 2);
+    }
+
+    auto it = _history.insert(_history.end(), e);
+    _insert(_arr, it, _arrCapacity);
     return true;
 }
 
-
 template<typename T, typename Hasher>
 bool LinkedHashSet<T, Hasher>::remove(const T &e) {
-    if (arr_capacity_ * (1 - CAPACITY_COEFF_) > history_.size() && arr_capacity_ > DEFAULT_CAPACITY_) {
-        resize_(arr_capacity_ / 2);
+    size_t pos = _getHashPos(e, _arrCapacity);
+    std::list<Entry> *cur_list = _arr[pos];
+
+    if (cur_list == nullptr) {
+        return false;
     }
 
-    size_t pos = get_hash_pos_(e, arr_capacity_);
-    std::list<Entry> *cur_list = arr_[pos];
-
-    if (cur_list == nullptr)
-        return false;
-
-    auto curEntryIter = is_in_list_(cur_list, e);
+    auto curEntryIter = _findInList(cur_list, e);
 
     if (curEntryIter == cur_list->end()) {
         return false;
     } else {
-        history_.erase(curEntryIter->iterator_);
-        cur_list->erase(curEntryIter);
+        _history.erase(curEntryIter->_iterator);
+        if (_arrCapacity * (1 - CAPACITY_COEFF) > _history.size() && _arrCapacity > DEFAULT_CAPACITY) {
+            _resize(_arrCapacity / 2);
+        } else {
+            cur_list->erase(curEntryIter);
+        }
         return true;
     }
 }
 
 template<typename T, typename Hasher>
 void LinkedHashSet<T, Hasher>::swap(LinkedHashSet &other) {
-    std::swap(arr_, other.arr_);
-    std::swap(arr_capacity_, other.arr_capacity_);
-    std::swap(history_, other.history_);
-    std::swap(hasher_, other.hasher_);
+    std::swap(_arr, other._arr);
+    std::swap(_arrCapacity, other._arrCapacity);
+    std::swap(_history, other._history);
+    std::swap(_hasher, other._hasher);
 }
 
 template<typename T, typename Hasher>
 size_t LinkedHashSet<T, Hasher>::size() const {
-    return history_.size();
+    return _history.size();
 }
 
 template<typename T, typename Hasher>
 bool LinkedHashSet<T, Hasher>::empty() const {
-    return history_.empty();
+    return _history.empty();
 }
 
 
 template<typename T, typename Hasher>
-bool LinkedHashSet<T, Hasher>::contains(const T &e) const {
-    size_t pos = get_hash_pos_(e, arr_capacity_);
-    std::list<Entry> *list = arr_[pos];
-    return list != nullptr && is_in_list_(list, e) != list->end();
+bool LinkedHashSet<T, Hasher>::contains(const T &e) {
+    return find(e) != this->end();
 }
 
 template<typename T, typename Hasher>
 LinkedHashSet<T, Hasher> &LinkedHashSet<T, Hasher>::clear() {
-    delete_arr_();
-    arr_capacity_ = DEFAULT_CAPACITY_;
-    arr_ = new std::list<Entry> *[DEFAULT_CAPACITY_]();
-    history_.clear();
+    _deleteArr();
+    _arrCapacity = DEFAULT_CAPACITY;
+    _arr = new std::list<Entry> *[DEFAULT_CAPACITY]();
+    _history.clear();
     return *this;
 }
 
 template<typename T, typename Hasher>
-bool LinkedHashSet<T, Hasher>::operator==(const LinkedHashSet &other) {
+bool LinkedHashSet<T, Hasher>::operator==(LinkedHashSet &other) {
     if (this->size() != other.size()) {
         return false;
     }
@@ -114,92 +113,98 @@ bool LinkedHashSet<T, Hasher>::operator==(const LinkedHashSet &other) {
 }
 
 template<typename T, typename Hasher>
-bool LinkedHashSet<T, Hasher>::operator!=(const LinkedHashSet &other) {
+bool LinkedHashSet<T, Hasher>::operator!=(LinkedHashSet &other) {
     return !operator==(other);
 }
 
 template<typename T, typename Hasher>
 typename std::list<T>::iterator LinkedHashSet<T, Hasher>::find(const T &e) {
-    size_t pos = get_hash_pos_(e);
-    std::list<LinkedHashSet<T, Hasher>::Entry> list = arr_[pos];
-    return is_in_list_(list, e);
+    size_t pos = _getHashPos(e, _arrCapacity);
+    std::list<LinkedHashSet<T, Hasher>::Entry> *list = _arr[pos];
+    if (list == nullptr) {
+        return this->end();
+    }
+    auto entryIter = _findInList(list, e);
+    return entryIter == list->end() ? this->end() : entryIter->_iterator;
 }
 
 template<typename T, typename Hasher>
 typename std::list<T>::iterator LinkedHashSet<T, Hasher>::begin() {
-    return history_.begin();
+    return _history.begin();
 }
 
 template<typename T, typename Hasher>
 typename std::list<T>::iterator LinkedHashSet<T, Hasher>::end() {
-    return history_.end();
+    return _history.end();
 }
 
 template<typename T, typename Hasher>
-size_t LinkedHashSet<T, Hasher>::get_hash_pos_(const T &e, size_t capacity) const {
-    return hasher_(e) % capacity;
+size_t LinkedHashSet<T, Hasher>::_getHashPos(const T &e, size_t capacity) const {
+    return _hasher(e) % capacity;
 }
 
 template<typename T, typename Hasher>
-void LinkedHashSet<T, Hasher>::resize_(size_t new_capacity) {
-    delete_arr_();
-    auto new_arr = new std::list<Entry> *[new_capacity]();
-    for (auto it = history_.begin(); it != history_.end(); ++it) {
-        insert_(new_arr, *it, it, new_capacity);
+void LinkedHashSet<T, Hasher>::_resize(size_t newCapacity) {
+    _deleteArr();
+    auto new_arr = new std::list<Entry> *[newCapacity]();
+    for (auto it = _history.begin(); it != _history.end(); ++it) {
+        _insert(new_arr, it, newCapacity);
     }
-    arr_ = new_arr;
-    arr_capacity_ = new_capacity;
+    _arr = new_arr;
+    _arrCapacity = newCapacity;
 }
 
 template<typename T, typename Hasher>
-inline void LinkedHashSet<T, Hasher>::delete_arr_() {
-    if (this->arr_ == nullptr)
+inline void LinkedHashSet<T, Hasher>::_deleteArr() {
+    if (this->_arr == nullptr) {
         return;
-
-    for (size_t i = 0; i < arr_capacity_; i++) {
-        delete arr_[i];
     }
-    delete[] this->arr_;
+
+    for (size_t i = 0; i < _arrCapacity; i++) {
+        delete _arr[i];
+    }
+    delete[] this->_arr;
 }
 
 template<typename T, typename Hasher>
 inline typename std::list<typename LinkedHashSet<T, Hasher>::Entry>::iterator
-LinkedHashSet<T, Hasher>::is_in_list_(std::list<Entry> *list, const T &e) const {
+LinkedHashSet<T, Hasher>::_findInList(std::list<Entry> *list, const T &e) const {
     assert(list != nullptr);
-    return std::find_if(list->begin(), list->end(), [e](const Entry &x) { return x.value_ == e; });
+    return std::find_if(list->begin(), list->end(), [e](const Entry &x) { return x._value == e; });
 }
 
 template<typename T, typename Hasher>
 inline std::list<typename LinkedHashSet<T, Hasher>::Entry> &
-LinkedHashSet<T, Hasher>::get_list(std::list<Entry> **arr, size_t pos) {
-    std::list<typename LinkedHashSet<T, Hasher>::Entry> *cur_list = arr[pos];
-    if (cur_list == nullptr) {
+LinkedHashSet<T, Hasher>::_getList(std::list<Entry> **arr, size_t pos) {
+    assert(arr != nullptr);
+    std::list<Entry> *curList = arr[pos];
+    if (curList == nullptr) {
         arr[pos] = new std::list<typename LinkedHashSet<T, Hasher>::Entry>();
-        cur_list = arr[pos];
+        curList = arr[pos];
     }
-    return *cur_list;
+    return *curList;
 }
 
 template<typename T, typename Hasher>
-void LinkedHashSet<T, Hasher>::insert_(std::list<Entry> **arr, const T &e, typename std::list<T>::iterator &it,
-                                       size_t capacity) {
-    size_t pos = get_hash_pos_(e, capacity);
-    std::list<Entry> &cur_list = get_list(arr, pos);
-    cur_list.emplace_back(Entry(*it, it));
+void
+LinkedHashSet<T, Hasher>::_insert(std::list<Entry> **arr, typename std::list<T>::iterator &it, size_t capacity) {
+    size_t pos = _getHashPos(*it, capacity);
+    std::list<Entry> &curList = _getList(arr, pos);
+    curList.emplace_back(Entry(*it, it));
 }
 
 template<typename T, typename Hasher>
-LinkedHashSet<T, Hasher>::Entry::Entry(T &value, typename std::list<T>::iterator &iterator) : value_(value),
-                                                                                              iterator_(iterator) {
+LinkedHashSet<T, Hasher>::Entry::Entry(T &value, typename std::list<T>::iterator &iterator) : _value(value),
+                                                                                              _iterator(iterator) {
 }
 
 template<typename T, typename Hasher>
-LinkedHashSet<T, Hasher>::Entry::Entry(const LinkedHashSet::Entry &other) : value_(other.value_),
-                                                                            iterator_(other.iterator_) {}
+LinkedHashSet<T, Hasher>::Entry::Entry(const LinkedHashSet::Entry &other) : _value(other._value),
+                                                                            _iterator(other._iterator) {}
 
 template<typename T, typename Hasher>
 bool LinkedHashSet<T, Hasher>::Entry::operator==(const LinkedHashSet::Entry &other) const {
-    return value_ == other.value_ && iterator_ == other.iterator_;
+    return _value == other._value && _iterator == other._iterator;
 }
 
 template<typename T, typename Hasher>
